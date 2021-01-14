c13eea3a6beeef826645a54819019956c039d114
EvanRPavone <evanrpavone@gmail.com>
2021-01-14 11:19:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = 9%5;
+
+const max = 20;
